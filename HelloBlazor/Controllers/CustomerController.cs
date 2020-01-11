2b41967437fc544ea6f19ac9f26fc985cd14fdb2
BuddhikaNelum <dlbnprabath@gmail.com>
2020-01-11 22:59:34 +0530

    Added Customer Controller

---
+++ HelloBlazor/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloBlazor.Models;
+using HelloBlazor.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloBlazor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            this.customerService = customerService;
+        }
+
+        [HttpGet]
+        [Route("GetAllCustomers")]
+        public async Task<IEnumerable<Customer>> GetAllCustomers()
+        {
+            return await customerService.GetAllCustomers();
+        }
+    }
+}
